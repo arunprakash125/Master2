0f3d7486ac6932d74d8f36497712a32e2e82b7cf
arunprakash125 <arunprakash125@gmail.com>
2022-03-04 10:54:43 +0530

    message

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,12 @@
+package org.computer;
+
+public class Desktop extends Computer{
+private void desktopSize() {
+System.out.println("desktop size is 15inch");
+}
+public static void main(String[] args) {
+	Desktop d = new Desktop();
+	d.desktopSize();
+	d.computerModel();
+}
+}
